c3ab99c2c088b613066fb4c1f2f2ca57462a5c3b
Daniel Berg <Berg.iodaniel@gmail.com>
2022-01-10 21:01:09 +0100

    mostly done

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 let myLibrary = [];
 
-function Book(title, author, pages, ) {
+function Book(title, author, pages) {
     this.title = '"' + title + '"';
     this.author = author;
     this.pages = pages;
+    this.id = '"' + title + '"' + author + "Pages: " + pages;
+    this.isRead = false;
 }
 
 function addBookToLibrary(book) {
@@ -11,63 +13,122 @@ function addBookToLibrary(book) {
 }
 
 addBookToLibrary(new Book('Harry Potter', 'JK Rowling', 400));
-addBookToLibrary(new Book("Ice and fire", "FishMan", 200))
+addBookToLibrary(new Book("Ice and fire", "FishMan", 200));
+
+
+function buildCard(book) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const cardTitle = document.createElement('h2');
+    cardTitle.classList.add('card-title');
+    cardTitle.innerText = book.title;
+    const cardAuthor = document.createElement('h3');
+    cardAuthor.classList.add('card-author');
+    cardAuthor.innerText = book.author;
+    const cardPages = document.createElement('p');
+    cardPages.classList.add('card-pages');
+    cardPages.innerText = "Pages: " + book.pages;
+
+    const cardButtons = document.createElement('div');
+    cardButtons.classList.add('card-buttons');
+    const readButton = document.createElement('button');
+    readButton.classList.add('read-button');
+    readButton.classList.add('button');
+    readButton.innerText = 'unread';
+    readButton.style.backgroundColor = 'yellow';
+    readButton.addEventListener('click', (e) => toggleRead(e));
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    deleteButton.classList.add('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.addEventListener('click', (e) => deleteCard(e));
+
+    card.appendChild(cardTitle);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardPages);
+    card.appendChild(cardButtons);
+    cardButtons.appendChild(readButton);
+    cardButtons.appendChild(deleteButton);
+
+    return card;
+}
 
 function DisplayBooks() {
     const cardContainer = document.querySelector('#BookCardsContainer');
 
     myLibrary.forEach(book => { 
-        const card = document.createElement('div');
-        card.classList.add('card');
-
-        const cardTitle = document.createElement('h2');
-        cardTitle.classList.add('card-title');
-        cardTitle.innerText = book.title;
-        const cardAuthor = document.createElement('h3');
-        cardAuthor.classList.add('card-author');
-        cardAuthor.innerText = book.author;
-        const cardPages = document.createElement('p');
-        cardPages.classList.add('card-pages');
-        cardPages.innerText = book.pages;
-
-        const cardButtons = document.createElement('div');
-        cardButtons.classList.add('card-buttons');
-        const readButton = document.createElement('button');
-        readButton.classList.add('read-button');
-        readButton.innerText = 'unread';
-        readButton.style.backgroundColor = 'yellow';
-        const deleteButton = document.createElement('button');
-        deleteButton.classList.add('delete-button');
-        deleteButton.innerText = 'Delete';
-
-        card.appendChild(cardTitle);
-        card.appendChild(cardAuthor);
-        card.appendChild(cardPages);
-        card.appendChild(cardButtons);
-        cardButtons.appendChild(readButton);
-        cardButtons.appendChild(deleteButton);
-
+        let card = buildCard(book);
         cardContainer.appendChild(card);
     })
 }
 
+function resetCards() {
+    const cardContainer = document.querySelector('#BookCardsContainer');
+    cardContainer.innerHTML = "";
+    DisplayBooks();
+}
+
 DisplayBooks();
 
+function createBook() {
+    let form = document.querySelector('#creatBookForm');
+    
+    myLibrary.push(new Book(form.titleInput.value, form.authorInput.value, form.pagesInput.value));
+    resetCards();
+}
 
-document.querySelectorAll('.read-button').forEach(btn => btn.addEventListener('click', (e) => {
+
+function toggleRead(e) {
+    let id = e.target.parentNode.parentNode.children[0].innerText + e.target.parentNode.parentNode.children[1].innerText + e.target.parentNode.parentNode.children[2].innerText;
     if (e.target.innerText == 'read') {
         e.target.innerText = 'unread';
         e.target.style.backgroundColor = 'yellow';
+        let index = myLibrary.findIndex(book => book.id === id);
+        myLibrary[index].isRead = true;
     }
     else {
         e.target.innerText = 'read';
         e.target.style.backgroundColor = 'lightgreen';
+        let index = myLibrary.findIndex(book => book.id === id);
+        
+        myLibrary[index].isRead = false;
     }
-}));
+}
 
-document.querySelectorAll('.delete-button').forEach(btn => btn.addEventListener('click', (e) => {
-    // Delete the element
+function deleteCard(e) {
     const cardContainer = document.querySelector('#BookCardsContainer');
-    cardContainer.removeChild(e.target.parentNode.parentNode);
+    let id = e.target.parentNode.parentNode.children[0].innerText + e.target.parentNode.parentNode.children[1].innerText + e.target.parentNode.parentNode.children[2].innerText;
     
-}));
+    let index = myLibrary.findIndex(book => book.id === id);
+
+    myLibrary.splice(index, 1);
+    cardContainer.removeChild(e.target.parentNode.parentNode);
+}
+
+let formOpened = false;
+
+document.getElementById('Btn-CreateBook').addEventListener('click', () => {
+    if (formOpened) {
+        closeBookForm();
+    }
+    else {
+        openBookForm();
+    }
+})
+
+document.querySelector('#submitCreatBookBtn').addEventListener('click', closeBookForm);
+
+function openBookForm() {
+    document.getElementById('popupFormCreateBook').style.display = "block";
+    document.getElementById('dimmer').style.display = 'block';
+    formOpened = true;
+}
+
+document.querySelector('#dimmer').addEventListener('click', closeBookForm);
+
+function closeBookForm() {
+    document.getElementById('popupFormCreateBook').style.display = "none";
+    document.getElementById('dimmer').style.display = 'none';
+    formOpened = false;
+}
